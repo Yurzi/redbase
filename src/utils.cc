7e52f1b396203f60a95b6d7fc5dc29a046db3a7c
Yurzi <43744366+Yurzi@users.noreply.github.com>
2022-10-20 23:50:50 +0800

    rename utils functions

--- src/utils.cc
+++ src/utils.cc
@@ -15,65 +15,65 @@ void unix_error(const char *msg) {
   exit(-1);
 }
 
-int32_t Open(const char *pathname, int32_t flags) {
-  int32_t rc = open(pathname, flags);
+int32_t open(const char *pathname, int32_t flags) {
+  int32_t rc = ::open(pathname, flags);
   if (rc < 0)
     unix_error("Open error");
 
   return rc;
 }
 
-int32_t Open(const char *pathname, int32_t flags, mode_t mode) {
-  int32_t rc = open(pathname, flags, mode);
+int32_t open(const char *pathname, int32_t flags, mode_t mode) {
+  int32_t rc = ::open(pathname, flags, mode);
   if (rc < 0)
     unix_error("Open error");
 
   return rc;
 }
 
-ssize_t Read(int32_t fd, void *buf, size_t count) {
-  ssize_t rc = read(fd, buf, count);
+ssize_t read(int32_t fd, void *buf, size_t count) {
+  ssize_t rc = ::read(fd, buf, count);
   if (rc < 0)
     unix_error("Read error");
 
   return rc;
 }
 
-ssize_t Write(int32_t fd, const void *buf, size_t count) {
-  ssize_t rc = write(fd, buf, count);
+ssize_t write(int32_t fd, const void *buf, size_t count) {
+  ssize_t rc = ::write(fd, buf, count);
   if (rc < 0)
     unix_error("Write error");
 
   return rc;
 }
 
-off_t Lseek(int32_t fd, off_t offset, int32_t whence) {
-  off_t rc = lseek(fd, offset, whence);
+off_t lseek(int32_t fd, off_t offset, int32_t whence) {
+  off_t rc = ::lseek(fd, offset, whence);
   if (rc < 0)
     unix_error("Lssk error");
 
   return rc;
 }
 
-void Close(int32_t fd) { int32_t rc = close(fd); }
+void close(int32_t fd) { int32_t rc = ::close(fd); }
 
-int32_t Unlink(const char *pathname) {
-  int32_t rc = unlink(pathname);
+int32_t unlink(const char *pathname) {
+  int32_t rc = ::unlink(pathname);
   if (rc < 0)
     unix_error("Unlink error");
 
   return 0;
 }
 
-char *Getcwd(char *buf, size_t size) {
-  char *src = getcwd(buf, size);
+char *getcwd(char *buf, size_t size) {
+  char *src = ::getcwd(buf, size);
   if (src == nullptr)
     unix_error("Getcwd error");
   return src;
 }
 
-int32_t Chdir(const char *path) {
-  int32_t rc = chdir(path);
+int32_t chdir(const char *path) {
+  int32_t rc = ::chdir(path);
   if (rc < 0)
     unix_error("Chdir error");
   return 0;
--- src/utils.h
+++ src/utils.h
@@ -6,15 +6,15 @@
 #include <sys/types.h>
 
 namespace redbase {
-int32_t Open(const char *pathname, int32_t flags);
-int32_t Open(const char *pathname, int32_t flags, mode_t mode);
-ssize_t Read(int32_t fd, void *buf, size_t count);
-ssize_t Write(int32_t fd, const void *buf, size_t count);
-off_t Lseek(int32_t fd, off_t offset, int32_t whence);
-void Close(int32_t fd);
-int32_t Unlink(const char *pathname);
-char *Getcwd(char *buf, size_t size);
-int32_t Chdir(const char *path);
+int32_t open(const char *pathname, int32_t flags);
+int32_t open(const char *pathname, int32_t flags, mode_t mode);
+ssize_t read(int32_t fd, void *buf, size_t count);
+ssize_t write(int32_t fd, const void *buf, size_t count);
+off_t lseek(int32_t fd, off_t offset, int32_t whence);
+void close(int32_t fd);
+int32_t unlink(const char *pathname);
+char *getcwd(char *buf, size_t size);
+int32_t chdir(const char *path);
 }  // namespace redbase
 
 #endif  // ! __UTILS_H__
